48d99405c8c368e6a38a7a7885ddaefbeb84d622
Zack Beyer <zkbeyer@yahoo.com>
2026-01-21 12:33:59 -0600

    Add Hello World app & screenshot

---
+++ HelloWorldApp/HelloWorldApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by Zack Beyer on 1/21/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HelloWorldApp/HelloWorldAppTests/HelloWorldAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldAppTests.swift
+//  HelloWorldAppTests
+//
+//  Created by Zack Beyer on 1/21/26.
+//
+
+import Testing
+@testable import HelloWorldApp
+
+struct HelloWorldAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
